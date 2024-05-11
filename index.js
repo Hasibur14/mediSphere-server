fc14428a6b5bee93b51537e62ffd1ad1aabdd324
hasibur14 <hasib141415@gmail.com>
2024-05-11 23:47:25 +0600

    added single service data from db

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lfxjcnl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
@@ -33,6 +33,14 @@ async function run() {
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find()
             const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        // Get a single service data from db using service id
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await serviceCollection.findOne(query)
             res.send(result)
         })
 
