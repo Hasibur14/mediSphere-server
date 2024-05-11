bd36bee8b0ef31d41e15a5ec2891b07df73ce17e
hasibur14 <hasib141415@gmail.com>
2024-05-11 12:36:38 +0600

    added node js and express js in local server

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-const express = require('express');
+const express = require('express'); 
 const cors = require('cors');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('mediSphere is Running')
+})
+
+app.listen(port, () => {
+    console.log(`server running on port ${port}`)
+})
